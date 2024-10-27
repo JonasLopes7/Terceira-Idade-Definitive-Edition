75c4b0276199d3efc8b3555daa5ef3fccc297401
JonasLopes7 <jonassub12@gmail.com>
2024-10-27 00:02:35 -0300

    Sistema de Tutorial Completo
    Todos os tutoriais estão prontos e com o sistema totalmente funcional. O jogo detecta se é a primeira vez do jogador no minigame e caso seja, ele apresenta o tutorial antes do minigame. Após jogar 1 vez, o botao leva direto para o minigame, mas existe um botao para ver o tutorial novamente. Tutorial já tem animações.

--- Assets/Scripts/SceneMan.cs
+++ Assets/Scripts/SceneMan.cs
@@ -7,11 +7,32 @@ public class SceneMan : MonoBehaviour
 {
     public List<string> sceneNames;
 
+    public string tutorialSceneMinigame1 = "TutorialMinigame1";
+    public string tutorialSceneMinigame2 = "TutorialMinigame2";
+
+    public int minigame1Index;
+    public int minigame2Index;
+
     public void ChangeSceneByIndex(int index)
     {
         if (index >= 0 && index < sceneNames.Count)
         {
-            SceneManager.LoadScene(sceneNames[index]);
+            if (index == minigame1Index && PlayerPrefs.GetInt("HasSeenTutorialMinigame1", 0) == 0)
+            {
+                PlayerPrefs.SetInt("HasSeenTutorialMinigame1", 1);
+                PlayerPrefs.Save();
+                SceneManager.LoadScene(tutorialSceneMinigame1);
+            }
+            else if (index == minigame2Index && PlayerPrefs.GetInt("HasSeenTutorialMinigame2", 0) == 0)
+            {
+                PlayerPrefs.SetInt("HasSeenTutorialMinigame2", 1);
+                PlayerPrefs.Save();
+                SceneManager.LoadScene(tutorialSceneMinigame2);
+            }
+            else
+            {
+                SceneManager.LoadScene(sceneNames[index]);
+            }
         }
         else
         {
@@ -24,4 +45,12 @@ public class SceneMan : MonoBehaviour
         Application.Quit();
         Debug.Log("Game is exiting");
     }
+
+    public void ResetTutorials()
+{
+    PlayerPrefs.DeleteKey("HasSeenTutorialMinigame1");
+    PlayerPrefs.DeleteKey("HasSeenTutorialMinigame2");
+    PlayerPrefs.Save();
+    Debug.Log("Status dos tutoriais foi resetado.");
+}
 }
---
+++ Assets/Scripts/TutorialMan.cs
@@ -0,0 +1,88 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TutorialMan : MonoBehaviour
+{
+   [System.Serializable]
+    public class TutorialStep
+    {
+        [TextArea] public string instructionText;
+        public GameObject minigameAnimation;
+        public Transform[] targetPositions;
+        public GameObject[] objectsToActivate;
+        public GameObject[] objectsToDeactivate;
+    }
+
+    public TutorialStep[] tutorialSteps;
+    public Text instructionTextBox;
+    public GameObject[] minigameAnimations;
+    public GameObject[] objectsToMove;
+
+    public GameObject ultimoObj;
+    public GameObject botaoCena;
+
+    private int currentStep = 0;
+
+    void Start()
+    {
+        UpdateTutorialStep();
+    }
+
+    public void OnClickNextStep()
+    {
+        if (currentStep < tutorialSteps.Length - 1)
+        {
+            currentStep++;
+            UpdateTutorialStep();
+        }
+        else
+        {
+            EndTutorial();
+        }
+    }
+
+    void UpdateTutorialStep()
+    {
+        instructionTextBox.text = tutorialSteps[currentStep].instructionText;
+
+        foreach (var animation in minigameAnimations)
+        {
+            animation.SetActive(false);
+        }
+
+        tutorialSteps[currentStep].minigameAnimation.SetActive(true);
+
+        for (int i = 0; i < objectsToMove.Length; i++)
+        {
+            if (i < tutorialSteps[currentStep].targetPositions.Length)
+            {
+                objectsToMove[i].transform.position = tutorialSteps[currentStep].targetPositions[i].position;
+            }
+        }
+
+        foreach (var obj in tutorialSteps[currentStep].objectsToActivate)
+        {
+            obj.SetActive(true);
+        }
+
+        foreach (var obj in tutorialSteps[currentStep].objectsToDeactivate)
+        {
+            obj.SetActive(false);
+        }
+    }
+
+    void EndTutorial()
+    {
+        instructionTextBox.text = "Tutorial Completo!";
+
+        ultimoObj.SetActive(true);
+        botaoCena.SetActive(true);
+
+        foreach (var animation in minigameAnimations)
+        {
+            animation.SetActive(false);
+        }
+    }
+}

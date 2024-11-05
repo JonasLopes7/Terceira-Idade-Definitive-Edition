a15ec86f02803aa1c20e3d8c3810c77a1c45e47d
JonasLopes7 <jonassub12@gmail.com>
2024-11-05 10:56:59 -0300

    Animação arrumada
    Arrumei a animação do menu de seleção de minigames conforme o feedback do professor

--- Assets/Scripts/AnimatorMenu.cs
+++ Assets/Scripts/AnimatorMenu.cs
@@ -5,54 +5,21 @@ using UnityEngine.UI;
 
 public class AnimatorMenu : MonoBehaviour
 {
-    public GameObject panelPal;
-    public GameObject panelRel;
-    private Animator animatorPalavra;
-    private Animator animatorRelogio;
-    public CanvasGroup canvasGroup;
+    public GameObject panel;
+    private Animator animator;
 
     void Start()
     {
-        animatorPalavra = panelPal.GetComponent<Animator>();
-        animatorRelogio = panelRel.GetComponent<Animator>();
-        panelPal.SetActive(false);
-        panelRel.SetActive(false);
+        animator = panel.GetComponent<Animator>();
     }
 
-    public void ShowPanelPal()
+    public void ShowPanel()
     {
-        panelPal.SetActive(true);
-        animatorPalavra.Play("animacaoPainelPalavra");
-        canvasGroup.interactable = false;
+        animator.Play("anim1");
     }
 
-    public void HidePanelPal()
+    public void HidePanel()
     {
-        animatorPalavra.Play("animacaoPainelPalavraVolta");
-        Invoke("DisablePanelPal", 0.5f);
-        canvasGroup.interactable = true;
-    }
-
-    private void DisablePanelPal()
-    {
-        panelPal.SetActive(false); // Desativa o painel
-    }
-    public void ShowPanelRel()
-    {
-        panelRel.SetActive(true);
-        animatorRelogio.Play("animacaoPainelRelogio");
-        canvasGroup.interactable = false;
-    }
-
-    public void HidePanelRel()
-    {
-        animatorRelogio.Play("animacaoPainelRelogioVolta");
-        Invoke("DisablePanelRel", 0.5f);
-        canvasGroup.interactable = true;
-    }
-
-    private void DisablePanelRel()
-    {
-        panelRel.SetActive(false); // Desativa o painel
+        animator.Play("anim2");
     }
 }
